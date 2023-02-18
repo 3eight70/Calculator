4a8360f51bedb2e4eb2a915926447e77ddfb07c3
3eight70 <@gbhfns90@gmail.com>
2023-02-18 18:10:00 +0700

    first commit

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var binding=ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.myButton.setOnClickListener(){
+
+        }
+    }
+}
