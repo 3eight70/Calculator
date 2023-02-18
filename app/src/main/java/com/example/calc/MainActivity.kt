c86150f377707629c26c62d2359691ee60b302fc
3eight70 <@gbhfns90@gmail.com>
2023-02-18 19:15:10 +0700

    second commit

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calc.databinding.ActivityMainBinding
