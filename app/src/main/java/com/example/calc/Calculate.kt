38ceead1156a7bd578628fbf1d8ae06bf68000c1
3eight70 <@gbhfns90@gmail.com>
2023-02-27 21:20:27 +0700

    fourth commit

---
+++ app/src/main/java/com/example/calc/Calculate.kt
@@ -0,0 +1,202 @@
+package com.example.calc
+
+import androidx.core.content.ContextCompat
+
+class Calculate {
+    private var newNumberFlag=false
+    private var signs = listOf('+', '-','×', '÷')
+    private var curNumber=""
+    private var newNumber=""
+    private var text=""
+    private var sign:Char=' '
+    private var commaInCurNumber=false
+    private var commaInNewNumber=false
+    var main=MainActivity()
+
+    fun clear(): String{
+        curNumber=""
+        newNumber=""
+        newNumberFlag=false
+        commaInCurNumber=false
+        commaInNewNumber=false
+        return curNumber
+    }
+
+    fun removeLastChar(): String{
+        if (curNumber=="Error"){
+            clear()
+        }
+        else if (newNumber.isNotEmpty()){
+            if (newNumber.last()=='.'){
+                commaInNewNumber=false
+            }
+            newNumber=newNumber?.replaceFirst(".$".toRegex(), "").toString()
+            text=curNumber+newNumber
+            return text
+        }
+        else if (curNumber.isNotEmpty() && newNumber.isEmpty()){
+            if (curNumber.last()=='.'){
+                commaInCurNumber=false
+            }
+            curNumber=curNumber?.replaceFirst(".$".toRegex(), "").toString()
+            return curNumber
+        }
+        return ""
+    }
+
+    fun addSymbol(symbol:Char): String{
+        if ("Error" in curNumber){
+            clear()
+        }
+
+        if (curNumber.isNotEmpty() && (curNumber[curNumber.length-1] in signs || curNumber[curNumber.length-1]=='%') && (symbol in signs || symbol=='%') && newNumber.isEmpty()){
+            curNumber=curNumber.substring(0, curNumber.length-1)
+            curNumber+=symbol
+            return curNumber
+        }
+
+        else if ((symbol in signs || symbol=='%') && newNumber.isEmpty() && curNumber.isNotEmpty()){
+            curNumber+=symbol
+            newNumberFlag=true
+            return curNumber
+        }
+
+        else if (newNumberFlag==false){
+            if (curNumber.length>0 && symbol in signs){
+                curNumber+=symbol
+                return curNumber
+            }
+
+            else if (curNumber.length>0 && symbol==',' && commaInCurNumber==false){
+                curNumber+='.'
+                commaInCurNumber=true
+                return curNumber
+            }
+            else if (symbol !in signs && symbol!='%' && symbol!=','){
+                curNumber += symbol
+                return curNumber
+            }
+        }
+
+        else if (newNumber.isNotEmpty() && newNumberFlag==true && checkSign(symbol)){
+            curNumber = equal()
+            if (curNumber!="Error"){
+                curNumber+=symbol
+            }
+            return curNumber
+        }
+
+        else if (newNumberFlag==true && (curNumber[curNumber.length-1] in signs || curNumber[curNumber.length-1]=='%')){
+            if (symbol==',' && commaInNewNumber==false && newNumber.isNotEmpty()){
+                newNumber+='.'
+                commaInNewNumber=true
+                return curNumber+newNumber
+            }
+            else if (symbol!=',') {
+                newNumber += symbol
+                return curNumber + newNumber
+            }
+        }
+        else if (symbol!=','){
+            curNumber+=symbol
+            newNumberFlag=false
+            return curNumber
+        }
+        return curNumber+newNumber
+    }
+
+    fun checkSign(symbol: Char):Boolean{
+        if ((curNumber[curNumber.length-1] in signs || curNumber[curNumber.length-1]=='%') && (symbol in signs || symbol == '%') ){
+            sign=curNumber[curNumber.length-1]
+            curNumber=main.removeLastChar(curNumber).toString()
+            return true
+        }
+        return false
+    }
+
+    fun equal(): String{
+        var answer: Double=0.0
+        if (curNumber.last() !in signs && curNumber.last()!='%'){
+            answer=curNumber.toDouble()
+        }
+
+        if (curNumber[curNumber.length-1] in signs || curNumber[curNumber.length-1]=='%'){
+            sign=curNumber[curNumber.length-1]
+            curNumber=main.removeLastChar(curNumber).toString()
+            answer=curNumber.toDouble()
+        }
+        if (newNumber.isNotEmpty()) {
+            when (sign) {
+                '+' -> answer = curNumber.toDouble() + newNumber.toDouble()
+                '-' -> answer = curNumber.toDouble() - newNumber.toDouble()
+                '×' -> answer = curNumber.toDouble() * newNumber.toDouble()
+                '÷' -> answer = curNumber.toDouble() / newNumber.toDouble()
+                '%' -> answer = curNumber.toDouble() / 100 * newNumber.toDouble()
+            }
+        }
+        else{
+            when(sign){
+                '%'->answer=curNumber.toDouble()/100
+            }
+        }
+
+        newNumber=""
+        commaInNewNumber=false
+        curNumber=checkError(answer)
+        return curNumber
+    }
+
+    fun changeSign(): String{
+        if (newNumber.isEmpty()){
+            var temp:Char=' '
+            var flag=false
+            if (curNumber[curNumber.length-1] in signs || curNumber[curNumber.length-1]=='%'){
+                temp=curNumber[curNumber.length-1]
+                flag=true
+                curNumber=curNumber.substring(0,curNumber.length-1)
+            }
+            if (curNumber.toDouble()>0) {
+                curNumber = '-' + curNumber
+            }
+            else{
+                curNumber=curNumber.substring(1,curNumber.length)
+            }
+            if (flag==true) {
+                flag=false
+                curNumber+=temp
+                return curNumber
+            }
+            else{
+                flag=false
+                return curNumber
+            }
+        }
+        else{
+            curNumber=equal()
+            if (curNumber.toDouble()>0) {
+                curNumber = '-' + curNumber
+            }
+            else{
+                curNumber=curNumber.substring(1,curNumber.length)
+            }
+            curNumber='-'+curNumber
+            return curNumber
+        }
+    }
+
+    private fun setError(): String{
+        newNumberFlag=false
+        newNumber=""
+        curNumber="Error"
+        commaInCurNumber=false
+        commaInNewNumber=false
+        return curNumber
+    }
+
+    private fun checkError(number: Double): String{
+        if (number.isInfinite() || number.isNaN()){
+            return setError()
+        }
+        return number.toString()
+    }
+}
--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -3,93 +3,195 @@ package com.example.calc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.calc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+
+    fun removeLastChar(str: String?): String? {
+        return str?.replaceFirst(".$".toRegex(), "")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var calculate=Calculate()
         var binding=ActivityMainBinding.inflate(layoutInflater)
+        val normalColor= ContextCompat.getColor(this, R.color.input)
+        val errorColor=ContextCompat.getColor(this, R.color.inputError)
+
         val view = binding.root
         setContentView(view)
 
         binding.allButtons.buttonZero.setOnClickListener(){
-            binding.allButtons.input.append("0")
+            binding.allButtons.input.text=calculate.addSymbol('0')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonOne.setOnClickListener(){
-            binding.allButtons.input.append("1")
+            binding.allButtons.input.text=calculate.addSymbol('1')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonTwo.setOnClickListener(){
-            binding.allButtons.input.append("2")
+            binding.allButtons.input.text=calculate.addSymbol('2')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonThree.setOnClickListener(){
-            binding.allButtons.input.append("3")
+            binding.allButtons.input.text=calculate.addSymbol('3')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonFour.setOnClickListener(){
-            binding.allButtons.input.append("4")
+            binding.allButtons.input.text=calculate.addSymbol('4')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonFive.setOnClickListener(){
-            binding.allButtons.input.append("5")
+            binding.allButtons.input.text=calculate.addSymbol('5')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonSix.setOnClickListener(){
-            binding.allButtons.input.append("6")
+            binding.allButtons.input.text=calculate.addSymbol('6')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonSeven.setOnClickListener(){
-            binding.allButtons.input.append("7")
+            binding.allButtons.input.text=calculate.addSymbol('7')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonEight.setOnClickListener(){
-            binding.allButtons.input.append("8")
+            binding.allButtons.input.text=calculate.addSymbol('8')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
         binding.allButtons.buttonNine.setOnClickListener(){
-            binding.allButtons.input.append("9")
+            binding.allButtons.input.text=calculate.addSymbol('9')
+            binding.allButtons.input.setTextColor(normalColor)
         }
 
-        binding.allButtons.buttonAC.setOnClickListener(){
-            binding.allButtons.input.text=""
+        binding.allButtons.buttonAC.setOnClickListener() {
+            if (binding.allButtons.input.text.isNotEmpty()) {
+                binding.allButtons.input.text = calculate.clear()
+            }
         }
 
-        binding.allButtons.buttonPlus.setOnClickListener(){
-            binding.allButtons.input.append("+")
+        binding.allButtons.buttonPlus.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                if (binding.allButtons.input.text.last() == '+' || binding.allButtons.input.text.last() == '-' || binding.allButtons.input.text.last() == '÷' || binding.allButtons.input.text.last() == '×' || binding.allButtons.input.text.last() == '%') {
+                    binding.allButtons.input.text =
+                        removeLastChar(binding.allButtons.input.text.toString())
+                }
+                binding.allButtons.input.text=calculate.addSymbol('+')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonMinus.setOnClickListener(){
-            binding.allButtons.input.append("-")
+        binding.allButtons.buttonMinus.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                if (binding.allButtons.input.text.last() == '+' || binding.allButtons.input.text.last() == '-' || binding.allButtons.input.text.last() == '÷' || binding.allButtons.input.text.last() == '%' || binding.allButtons.input.text.last() == '×') {
+                    binding.allButtons.input.text =
+                        removeLastChar(binding.allButtons.input.text.toString())
+                }
+                binding.allButtons.input.text=calculate.addSymbol('-')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonMultiplication.setOnClickListener(){
-            binding.allButtons.input.append("×")
+        binding.allButtons.buttonMultiplication.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                if (binding.allButtons.input.text.last() == '+' || binding.allButtons.input.text.last() == '-' || binding.allButtons.input.text.last() == '÷' || binding.allButtons.input.text.last() == '×' || binding.allButtons.input.text.last() == '%') {
+                    binding.allButtons.input.text =
+                        removeLastChar(binding.allButtons.input.text.toString())
+                }
+                binding.allButtons.input.text=calculate.addSymbol('×')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonDivide.setOnClickListener(){
-            binding.allButtons.input.append("÷")
+        binding.allButtons.buttonDivide.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                if (binding.allButtons.input.text.last() == '+' || binding.allButtons.input.text.last() == '-' || binding.allButtons.input.text.last() == '÷' || binding.allButtons.input.text.last() == '×' || binding.allButtons.input.text.last() == '%') {
+                    binding.allButtons.input.text =
+                        removeLastChar(binding.allButtons.input.text.toString())
+                }
+                binding.allButtons.input.text=calculate.addSymbol('÷')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonPercent.setOnClickListener(){
-            binding.allButtons.input.append("%")
+        binding.allButtons.buttonPercent.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                if (binding.allButtons.input.text.last() == '+' || binding.allButtons.input.text.last() == '-' || binding.allButtons.input.text.last() == '÷' || binding.allButtons.input.text.last() == '×' || binding.allButtons.input.text.last() == '%') {
+                    binding.allButtons.input.text =
+                        removeLastChar(binding.allButtons.input.text.toString())
+                }
+                binding.allButtons.input.text=calculate.addSymbol('%')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonComma.setOnClickListener(){
-            binding.allButtons.input.append(",")
+        binding.allButtons.buttonComma.setOnClickListener() {
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty() && binding.allButtons.input.text.last()!='.') {
+                binding.allButtons.input.text=calculate.addSymbol(',')
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
-        binding.allButtons.buttonPlusMinus.setOnClickListener(){
-            binding.allButtons.input.append("±")
+        binding.allButtons.buttonPlusMinus.setOnClickListener() {
+            if (binding.allButtons.input.text.isNotEmpty()) {
+                binding.allButtons.input.setTextColor(normalColor)
+                binding.allButtons.input.text=calculate.changeSign()
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            }
         }
 
         binding.allButtons.backspace.setOnClickListener(){
-            binding.allButtons.input.text?.replaceFirst(".$".toRegex(),"")
+            binding.allButtons.input.setTextColor(normalColor)
+            if (binding.allButtons.input.text.isNotEmpty()) {
+                binding.allButtons.input.text =
+                    calculate.removeLastChar()
+            }
+        }
+
+        binding.allButtons.buttonEqual.setOnClickListener(){
+            try{
+                if (binding.allButtons.input.text.last()!='.') {
+                    binding.allButtons.input.text = calculate.equal()
+                }
+                if (binding.allButtons.input.text=="Error"){
+                    binding.allButtons.input.setTextColor(errorColor)
+                }
+            } catch (e:java.lang.Exception){
+                Log.d("Ошибка", "сообщение: ${e.message}")
+            }
         }
     }
 }
